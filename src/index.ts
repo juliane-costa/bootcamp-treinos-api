fd94baa3e107bfd61b875e3f277a49366be598aa
Juliane Costa <juliane.costa@ab-inbev.com>
2026-03-01 21:53:41 -0300

    "feat: add fastfy api"

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-console.log("Hello Juliane!");
+// Import the framework and instantiate it
+import Fastify from 'fastify'
+const fastify = Fastify({
+  logger: true
+})
+
+// Declare a route
+fastify.get('/', async function handler () {
+  return { hello: 'world' }
+})
+
+// Run the server!
+try {
+  await fastify.listen({ port: Number(process.env.PORT) || 3000 })
+} catch (err) {
+  fastify.log.error(err)
+  process.exit(1)
+}
