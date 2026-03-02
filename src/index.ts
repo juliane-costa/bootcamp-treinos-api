d48702e24cbdc9c367b7d934c538dc54e22a3e86
Juliane Costa <juliane.costa@ab-inbev.com>
2026-03-01 22:17:30 -0300

    "chore: add fastify swagger generation"

--- src/index.ts
+++ src/index.ts
@@ -1,37 +1,67 @@
 // Import the framework and instantiate it
-import Fastify from 'fastify'
-import { serializerCompiler, validatorCompiler, ZodTypeProvider } from 'fastify-type-provider-zod';
-import z from 'zod';
+import fastifySwagger from "@fastify/swagger";
+import fastifySwaggerUI from "@fastify/swagger-ui";
+import Fastify from "fastify";
+import {
+  jsonSchemaTransform,
+  serializerCompiler,
+  validatorCompiler,
+  ZodTypeProvider,
+} from "fastify-type-provider-zod";
+import z from "zod";
 
 const app = Fastify({
-  logger: true
-})
+  logger: true,
+});
 
 // Add schema validator and serializer
 app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
 
+//documentacao
+await app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: "Bootcamp Treinos API",
+      description: "API para o Bootcamp Treinos",
+      version: "1.0.0",
+    },
+    servers: [
+      {
+        description: "LocalHost",
+        url: "http://localhost:3000",
+      },
+    ],
+  },
+  transform: jsonSchemaTransform,
+});
+
+await app.register(fastifySwaggerUI, {
+  routePrefix: "/docs",
+});
+//documentacao
+
 app.withTypeProvider<ZodTypeProvider>().route({
-    method: 'GET',
-    url: '/',
-    schema: {
-        description: 'Hello World',
-        tags: ['hello-world'],
-        response: {
-            200: z.object({
-                message: z.string(),
-            }),
-        },
+  method: "GET",
+  url: "/",
+  schema: {
+    description: "Hello World",
+    tags: ["ABC"],
+    response: {
+      200: z.object({
+        message: z.string(),
+      }),
     },
-    handler: () => {
-        return { message: 'Hello World' };
-    },
-  });
+  },
+  handler: () => {
+    return { message: "Hello World" };
+  },
+});
 
 // Run the server!
 try {
-  await app.listen({ port: Number(process.env.PORT) || 3000 })
+  await app.listen({ port: Number(process.env.PORT) || 3000 });
 } catch (err) {
-  app.log.error(err)
-  process.exit(1)
+  app.log.error(err);
+  process.exit(1);
 }
