bbe8fcd4a855fe8f421519ab8faa59940ab6f13a
Juliane Costa <juliane.costa@ab-inbev.com>
2026-03-04 20:22:21 -0300

    feat: creat get user train

--- src/index.ts
+++ src/index.ts
@@ -14,6 +14,7 @@ import z from "zod";
 import { NotFoundError } from "./errors/index.js";
 import { auth } from "./lib/auth.js";
 import { homeRoutes } from "./routes/home.js";
+import { meRoutes } from "./routes/me.js";
 import { statsRoutes } from "./routes/stats.js";
 import { workoutPlanRoutes } from "./routes/workout-plan.js";
 
@@ -70,6 +71,7 @@ await app.register(fastifyApiReference, {
 await app.register(workoutPlanRoutes, { prefix: "/workout-plans" });
 await app.register(homeRoutes, { prefix: "/home" });
 await app.register(statsRoutes, { prefix: "/stats" });
+await app.register(meRoutes, { prefix: "/me" });
 //endregion routes
 
 app.withTypeProvider<ZodTypeProvider>().route({
