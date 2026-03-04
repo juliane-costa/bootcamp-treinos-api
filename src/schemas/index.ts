480a40388dbc8869e47d3f75c1f916d52beb38d1
Juliane Costa <juliane.costa@ab-inbev.com>
2026-03-04 19:59:58 -0300

    feat: creat get status use case

--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -113,6 +113,25 @@ export const GetHomeResponseSchema = z.object({
   ),
 });
 
+export const GetStatsQuerySchema = z.object({
+  from: z.iso.date(),
+  to: z.iso.date(),
+});
+
+export const GetStatsResponseSchema = z.object({
+  workoutStreak: z.number(),
+  consistencyByDay: z.record(
+    z.iso.date(),
+    z.object({
+      workoutDayCompleted: z.boolean(),
+      workoutDayStarted: z.boolean(),
+    }),
+  ),
+  completedWorkoutsCount: z.number(),
+  conclusionRate: z.number(),
+  totalTimeInSeconds: z.number(),
+});
+
 export const WorkoutPlanSchema = z.object({
   id: z.uuid(),
   name: z.string().trim().min(1),
